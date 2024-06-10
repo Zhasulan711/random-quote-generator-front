3646679b69247caabaed75a7a95f5009a1584f25
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-06-10 18:11:37 +0500

    adding animation and adaptive design

--- src/components/RandomQuoteGenerator/RandomQuoteGenerator.tsx
+++ src/components/RandomQuoteGenerator/RandomQuoteGenerator.tsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from "react";
 
 import "../../styles/root/RandomQuoteGenerator.scss";
 import { fetchQuote } from "../../api/quotes/route";
+import { LoadingIndicator } from "./LoadingIndicator";
 
 export const RandomQuoteGenerator = () => {
   const [quote, setQuote] = useState<string>("");
   const [author, setAuthor] = useState<string>("");
   const [randomNumber, setRandomNumber] = useState<number>(1);
   const [image, setImage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getQuote = async () => {
+      setIsLoading(true);
       try {
         const data = await fetchQuote(randomNumber);
         setQuote(data.content);
@@ -18,6 +21,8 @@ export const RandomQuoteGenerator = () => {
         setImage(`http://localhost:8000/images/${data.avatarPath}`);
       } catch (error: any) {
         console.error("Fetch error:", error);
+      } finally {
+        setTimeout(() => setIsLoading(false), 1000);
       }
     };
     getQuote();
@@ -26,6 +31,10 @@ export const RandomQuoteGenerator = () => {
   const handleClick = () => {
     setRandomNumber(Math.floor(Math.random() * 10 + 1));
   };
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <div className="quote-generator-wrapper">
